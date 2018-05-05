9b752f6b29d1972908db931e939cf1ca9602ca3d
himanshuc3 <himichhabra14@gmail.com>
2018-05-05 21:53:55 +0530

    First commit for Heroku deployment

---
+++ index.js
@@ -0,0 +1,12 @@
+//Nodejs uses commonJS moule system require('app)
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req,res) => {
+    res.send({pro: 'ject'})
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
