643b5a8c36009a86f14e28312735d053519b501c
himanshuc3 <himichhabra14@gmail.com>
2018-05-09 20:38:11 +0530

    Responsive functionality added

--- client/src/components/RightCarousel.js
+++ client/src/components/RightCarousel.js
@@ -18,26 +18,26 @@ export default class RightCarousel extends Component {
                 edgeEasing="easeInBounce"
                 speed={700}
                 renderBottomCenterControls={({ currentSlide }) => (
-                        <div></div>
+                        <div className="carousel_controls"></div>
                 )}
                 renderCenterRightControls={({ previousSlide }) => (
-                    <div></div>
+                    <div className="carousel_controls"></div>
                 )}
                 renderCenterLeftControls={({ previousSlide }) => (
-                    <div></div>
+                    <div className="carousel_controls"></div>
                 )}
                 wrapAround={true}
             >
                 <div className="carousel_slide">
-                    <img className="what_is_it_image" src={whatIsIt} alt="What is it?"/>
+                    <img className="what_is_it_image carousel_images" src={whatIsIt} alt="What is it?"/>
                     <h3>Live schedule your timetable with friends / colleagues.</h3>
                 </div>
                 <div className="carousel_slide">
-                    <img className="why_use_it_image" src={whyUseIt} alt="What is it?"/>
+                    <img className="why_use_it_image carousel_images" src={whyUseIt} alt="What is it?"/>
                     <h3>Use it to enhance productivity by avoiding schedule collision.</h3>
                 </div>
                 <div className="carousel_slide">
-                    <img className="ease_of_use_image" src={easeOfUse} alt="What is it?"/>
+                    <img className="ease_of_use_image carousel_images" src={easeOfUse} alt="What is it?"/>
                     <h3>Easy to use. Manage on the go. Make Remote life easier.</h3>
                 </div>
             </Carousel>
