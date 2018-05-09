629eff9691cfdb038390b75638a60c82f7610e24
himanshuc3 <himichhabra14@gmail.com>
2018-05-09 22:41:54 +0530

    ok

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if(process.env.NODE_ENV === 'production'){
     //if it doesn't recognize the file
     const path = require('path');
     app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
