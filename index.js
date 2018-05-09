b57193e48a69346579c75b71c3d585f462394677
himanshuc3 <himichhabra14@gmail.com>
2018-05-09 21:14:18 +0530

    ok

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req,res) => {
-    res.send({pro: 'ject!'})
-});
-
+if(process.env.NODE_ENV === 'production'){
+    //Express will serve up the index.html file
+    //if it doesn't recognize the file
+    const path = require('path');
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
