6f4719dfa557ff2ff93917216369ae4ae9f2a677
himanshuc3 <himichhabra14@gmail.com>
2018-05-15 00:20:01 +0530

    try#3

--- index.js
+++ index.js
@@ -37,13 +37,13 @@ app.post('/addEvent', (req,res)=>{
 
 if(process.env.NODE_ENV === 'production'){
     //Serving assets in production from buil directory
-    app.use(express.static('client/build'));
+    app.use(express.static('client/src'));
 
     //Express will serve up the index.html file
     //if it doesn't recognize the file
     const path = require('path');
     app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
     });
 }
 
