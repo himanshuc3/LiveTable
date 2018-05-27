90d70ea2e57e7881738546c88f148184c1fb1939
Himanshu Chhabra <himichhabra14@gmail.com>
2018-05-27 16:05:57 +0530

    Added login using passport

---
+++ server/routes/api/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/current_user', (req, res) => {
+    res.send(JSON.stringify(req.user))
+})
+
+module.exports = router
